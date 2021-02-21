bd7a1ea48a629f291d9c7be468f30b0357db6f04
Andrew Peattie <drewpeattie@hotmail.com>
2021-02-21 20:28:01 +0000

    total expense

--- db/spending_tracker.sql
+++ db/spending_tracker.sql
@@ -15,7 +15,7 @@ name VARCHAR(250)
 CREATE TABLE transactions(
 id SERIAL PRIMARY KEY,
 cash INT,
-value INT,
+value decimal,
 merchant_id INT REFERENCES merchants(id) ON DELETE CASCADE,
 tag_id INT REFERENCES tags(id) ON DELETE CASCADE
 );
